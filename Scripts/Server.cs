fd7be7da8498e46fd44dc908c97cc9677b912b5a
buckawk32 <buckawk32.bit@gmail.com>
2026-02-18 10:28:40 +0800

    Ported Old Unity Server to Godot
    Moved Old Unity Server Code to the Godot Server

--- Scripts/Server.cs
+++ Scripts/Server.cs
@@ -1,6 +1,6 @@
 using Godot;
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
@@ -28,6 +28,30 @@ public partial class Server : Node
 
         public async Task SaveDevId()
         {
+            string incomingMsg;
+            incomingMsg = await streamReader.ReadLineAsync();
+            
+            if (incomingMsg.StartsWith("Confirmation : ")) {
+                GD.Print($"NEW CLIENT: " + incomingMsg.Substring(11));
+                await streamWriter.WriteLineAsync($"  {incomingMsg.Substring(11)}  -- CONNECTED SUCCESFULLY ");
+                await streamWriter.FlushAsync();
+
+                ID = incomingMsg.Substring(11);
+            }
+            else
+            {
+                await streamWriter.WriteLineAsync("Please Send Confirmation ID");
+                await streamWriter.FlushAsync();
+
+                ID = "NONE";
+            }
+        }
+
+        public void Dispose() 
+        {
+            streamReader.Close();
+            streamWriter.Close();
+            clientRef.Close();
         }
     }
 
@@ -36,14 +60,107 @@ public partial class Server : Node
     private static bool isServerRunning { get; set; }
 
     private static int portNumber = 4001;
-    private static IPAddress host = IPAddress.Parse("1207.0.0.1")
+    private static IPAddress host = IPAddress.Parse("1207.0.0.1");
+    private static List<ClientData> clientList = new List<ClientData>();
 
+    private static readonly object _lock = new object();
+
+    public static async Task StartServer()
+    {
+        server = new TcpListener(host, portNumber);
+        server.Start();
+        GD.Print("Server started...");
+        isServerRunning = true;
+
+        while (true) {
+            var client = await server.AcceptTcpClientAsync();
+            GD.Print("Accept New Client...");
+            var currentTask = StartConnectionAsync(client);
+
+            if (currentTask.IsFaulted) {
+                currentTask.Wait();
+            }
+        }
+    }
+
+    private async static Task StartConnectionAsync(TcpClient client) 
+    {
+        ClientData clientData = new ClientData(client);
+        lock (_lock)
+        {
+            clientList.Add(clientData); 
+        }
+        Task taskID = Task.Run(async () => await clientData.SaveDevId());
+        if (taskID.IsFaulted) {
+            taskID.Wait();
+        }
+
+        try {
+            if (!string.IsNullOrEmpty(clientData.ID) && clientData.ID != "NONE") {
+                await HandleClientAsync(clientData);
+            }
+        }
+        catch (Exception e) {
+            GD.Print(e);
+        }
+        finally {
+            lock (_lock) {
+                clientList.Remove(clientData);
+            }
+            clientData.Dispose();
+        }
+    }
+
+    private static async Task HandleClientAsync(ClientData clientData) {
+        // GD.Print()
+        string clientMsg;
+
+        while (clientList.Count == 1)
+        {
+            await clientData.streamWriter.WriteLineAsync($"Placeholder");
+            await clientData.streamWriter.FlushAsync();
+
+            clientMsg = await clientData.streamReader.ReadLineAsync();
+            if (clientMsg.StartsWith("QUIT:") && clientMsg != null) {
+                await clientData.streamWriter.WriteLineAsync($"  {clientData.ID} -- QUIT SUCCESFULLY  ");
+                await clientData.streamWriter.FlushAsync();
+
+                break;
+            }
+        }
+        GD.Print($"CLIENT QUIT: {clientData.ID}");
+    }
+
+    public void KillAll()
+    {
+        GD.Print("Shutting Down...");
+
+        lock (_lock)
+        {
+            if (clientList.Count > 0) {
+                foreach (ClientData clientData in clientList) {
+                    clientData.Dispose();
+                    clientList.Remove(clientData);
+                }
+            }
+        }
+    }
 
 	public override void _Ready()
 	{
+        Task serverStart = Task.Run(async () => await StartServer());
+        if (serverStart.IsFaulted) {
+            serverStart.Wait();
+        }
 	}
 
 	public override void _Process(double delta)
 	{
+
 	}
+
+    public override void _ExitTree()
+    {
+        KillAll();    
+    }
 }
